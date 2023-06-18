5de2a51533381bb903a729dda2733da2154919cb
Mathema_dx <necmettinkaradeli@gmail.com>
2023-06-18 20:05:29 +0300

    updated 18.06.2023 20:05

--- src/test/java/runners/Runner.java
+++ src/test/java/runners/Runner.java
@@ -10,14 +10,15 @@ import org.junit.runner.RunWith;
                 "html:target/default-cucumber-reports.html",
                 "json:target/json-reports/cucumber.json",
                 "junit:target/xml-report/cucumber.xml",
-                "rerun:target/failedRerun.txt"
+                "rerun:target/failedRerun.txt",
+                "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"
         },
         monochrome = true,
         features = "./src/test/resources/features",//PATH OF FEATURES FOLDER
         glue = {"stepdefinitions","hooks"}, //PATH OF STEPDEFINITIONS FOLDER
         dryRun = false , //dryRun = true : RUNS AND GENERATES ONLY MISSING STEP DEFS.
                         // dryRun = false: RUN NORMAL. THIS IS DEFAULT VALUE
-        tags = "@excel_admin"
+        tags = "@parameterizing1 or @failed_test_case"
 
 )
 public class Runner {
